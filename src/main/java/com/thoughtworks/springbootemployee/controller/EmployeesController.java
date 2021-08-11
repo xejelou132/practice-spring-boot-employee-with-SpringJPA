431aa45d6fe91fe8b1b2132747dfe5f94dfae476
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-11 16:21:29 +0800

    [Pairprogramming][kitz]feat: implement number 4 get

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -34,12 +34,33 @@ public class EmployeesController {
     @GetMapping(params = {"gender"})
     public List<Employees> getEmployeesByGender(@RequestParam("gender") String givenGender) {
         employeesList.add(new Employees(1,"Angelo", 23,"male",1000));
-        employeesList.add(new Employees(2,"Angela", 26,"fe",900));
+        employeesList.add(new Employees(2,"Angela", 26,"female",900));
 
         return employeesList.stream()
                 .filter(employee -> employee.getGender().equals(givenGender))
                 .collect(Collectors.toList());
     }
-//"?gender={givenGender}"
+
+    @GetMapping(params = {"index" , "size"})
+    public List<Employees> getEmployeesByPagination(@RequestParam int index , @RequestParam int size ) {
+        employeesList.add(new Employees(1,"Angelo", 23,"male",1000));
+        employeesList.add(new Employees(2,"Angela", 26,"female",900));
+        employeesList.add(new Employees(3,"Angelo2", 23,"male",1000));
+        employeesList.add(new Employees(4,"Angela2", 26,"female",900));
+        employeesList.add(new Employees(5,"Angelo3", 23,"male",1000));
+        employeesList.add(new Employees(6,"Angela3", 26,"female",900));
+        employeesList.add(new Employees(7,"Angela3", 26,"female",900));
+        employeesList.add(new Employees(8,"Angela3", 26,"female",900));
+        employeesList.add(new Employees(9,"Angela3", 26,"female",900));
+        employeesList.add(new Employees(10,"Angela3", 26,"female",900));
+
+
+
+
+
+        return employeesList.stream().skip((long) (index - 1) * size)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
 
 }
