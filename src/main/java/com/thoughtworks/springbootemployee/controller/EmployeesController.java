f376f3d7f3dc094c2fba746708cf7ea058cf50a2
OnceRobert <robpurugganan@gmail.com>
2021-08-11 14:38:23 +0800

    [Springboot Practice][Angelo] feat: implemented get all employees

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Employees;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+    private List<Employees> employeesList = new ArrayList<>();
+    @GetMapping
+    public List<Employees> getAllEmployees() {
+        employeesList.add(new Employees(1,"Angelo", 23,"male",1000));
+        employeesList.add(new Employees(2,"Angela", 26,"female",900));
+
+        return employeesList;
+    }
+
+
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employees.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.springbootemployee.model;
+
+public class Employees {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public Employees(Integer id, String name, Integer age, String gender, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+
+}
