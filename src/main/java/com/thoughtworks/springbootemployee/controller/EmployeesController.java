de8185ff6dcd149309ddb961857a67e3d1cb1066
OnceRobert <robpurugganan@gmail.com>
2021-08-11 15:19:04 +0800

    [Springboot Practice][Angelo] feat: implemented get all employees by gender

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,13 +1,12 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employees;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -22,7 +21,7 @@ public class EmployeesController {
     }
 
     @GetMapping("/{employeeId}")
-    public Employees getAllEmployees(@PathVariable Integer employeeId) {
+    public Employees getEmployeeById(@PathVariable Integer employeeId) {
         employeesList.add(new Employees(1,"Angelo", 23,"male",1000));
         employeesList.add(new Employees(2,"Angela", 26,"female",900));
 
@@ -32,5 +31,15 @@ public class EmployeesController {
                 .orElse(null);
     }
 
+    @GetMapping(params = {"gender"})
+    public List<Employees> getEmployeesByGender(@RequestParam("gender") String givenGender) {
+        employeesList.add(new Employees(1,"Angelo", 23,"male",1000));
+        employeesList.add(new Employees(2,"Angela", 26,"fe",900));
+
+        return employeesList.stream()
+                .filter(employee -> employee.getGender().equals(givenGender))
+                .collect(Collectors.toList());
+    }
+//"?gender={givenGender}"
 
 }
