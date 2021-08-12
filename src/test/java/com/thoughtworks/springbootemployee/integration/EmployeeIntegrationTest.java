70ecf80234c9482affce52e427913f08cef9aa75
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-12 21:00:23 +0800

    [Springboot][Kitz]Test: added the test should_return_list_of_employees_when_get_all_employees_given_get_request

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.Repository.EmployeesRepo;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+
+    @Autowired
+    private EmployeesRepo employeeRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void should_return_list_of_employees_when_get_all_employees_given_get_request() throws Exception {
+        //given
+        Employee employee = new Employee(1, "joseph", 22, "male", 1000000 , 1);
+        employeeRepository.save(employee);
+
+        //when
+        //then
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("joseph"))
+                .andExpect(jsonPath("$[0].age").value(22))
+                .andExpect(jsonPath("$[0].gender").value("male"))
+                .andExpect(jsonPath("$[0].salary").value(1000000));
+    }
+    
+}
