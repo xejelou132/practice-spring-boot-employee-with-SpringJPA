ed0835e4154bd9ca158905958770c8939f33dd3f
Darlen Kitz Suizo <suizoda@oocl.com>
2021-08-12 22:19:16 +0800

    [Springboot][Kitz]Test: Added the test

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -85,5 +85,97 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.gender").value("male"))
                 .andExpect(jsonPath("$.salary").value(1000000));
     }
-    
+
+    @Test
+    void should_update_employee_when_put_specific_employee_given_put_employee_request_and_details() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee(1, "joseph", 22, "male", 1000000,1));
+        String updatedEmployeeAsJson = "{\n" +
+                "  \"name\": \"maria\",\n" +
+                "  \"age\": 19,\n" +
+                "  \"gender\": \"female\",\n" +
+                "  \"salary\": 200000\n" +
+                "}";
+
+        //when
+        //then
+        mockMvc.perform(put("/employees/" + employee.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedEmployeeAsJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.name").value("maria"))
+                .andExpect(jsonPath("$.age").value(19))
+                .andExpect(jsonPath("$.gender").value("female"))
+                .andExpect(jsonPath("$.salary").value(200000));
+    }
+
+    @Test
+    void should_delete_employee_when_delete_request_given_delete_employee_request() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee(1, "joseph", 22, "male", 1000000,1));
+
+        //when
+        //then
+        mockMvc.perform(delete("/employees/" + employee.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").doesNotExist())
+                .andExpect(jsonPath("$[0].name").doesNotExist())
+                .andExpect(jsonPath("$[0].age").doesNotExist())
+                .andExpect(jsonPath("$[0].gender").doesNotExist())
+                .andExpect(jsonPath("$[0].salary").doesNotExist());
+    }
+    @Test
+    void should_return_list_of_specific_employees_when_get_employees_given_get_request_for_specific_gender() throws Exception {
+        //given
+        Employee maleEmployeeOne = new Employee(1, "joseph", 22, "male", 1000000,1);
+        Employee femaleEmployeeOne = new Employee(2, "maria", 19, "female", 200000 ,1);
+        Employee maleEmployeeTwo = new Employee(3, "jerick", 25, "male", 500 , 1);
+        employeeRepository.save(maleEmployeeOne);
+        employeeRepository.save(femaleEmployeeOne);
+        employeeRepository.save(maleEmployeeTwo);
+
+        //when
+        //then
+        mockMvc.perform(get("/employees?gender=male"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("joseph"))
+                .andExpect(jsonPath("$[0].age").value(22))
+                .andExpect(jsonPath("$[0].gender").value("male"))
+                .andExpect(jsonPath("$[0].salary").value(1000000))
+                .andExpect(jsonPath("$[1].id").isNumber())
+                .andExpect(jsonPath("$[1].name").value("jerick"))
+                .andExpect(jsonPath("$[1].age").value(25))
+                .andExpect(jsonPath("$[1].gender").value("male"))
+                .andExpect(jsonPath("$[1].salary").value(500))
+                .andExpect(jsonPath("$[2].id").doesNotExist());
+    }
+
+    @Test
+    void should_return_page_1_and_2_for_employees_when_pagination_given_page_size_1_page_size_2() throws Exception {
+        //given
+        Employee maleEmployeeOne = new Employee(1, "joseph", 22, "male", 1000000, 1);
+        Employee femaleEmployeeOne = new Employee(2, "maria", 19, "female", 200000,1);
+        Employee maleEmployeeTwo = new Employee(3, "jerick", 25, "male", 500 ,1);
+        employeeRepository.save(maleEmployeeOne);
+        employeeRepository.save(femaleEmployeeOne);
+        employeeRepository.save(maleEmployeeTwo);
+
+        // when
+        // then
+        mockMvc.perform(get("/employees?page=1&pageSize=5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("joseph"))
+                .andExpect(jsonPath("$[0].age").value(22))
+                .andExpect(jsonPath("$[0].gender").value("male"))
+                .andExpect(jsonPath("$[0].salary").value(1000000))
+                .andExpect(jsonPath("$[1].id").isNumber())
+                .andExpect(jsonPath("$[1].name").value("maria"))
+                .andExpect(jsonPath("$[1].age").value(19))
+                .andExpect(jsonPath("$[1].gender").value("female"))
+                .andExpect(jsonPath("$[1].salary").value(200000));
+
+    }
 }
